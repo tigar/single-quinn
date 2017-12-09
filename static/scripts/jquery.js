240c46e6a4d58f604d73fabbaf8a4e0ac229f1bb
alcofrisbas <alcofrisbas@github.com>
2017-12-08 16:26:08 -0800

    more good looks

--- static/scripts/jquery.js
+++ static/scripts/jquery.js
@@ -1,15 +1,15 @@
 $(document).ready(function (){
 	$("#top").click(function (){
 		$('html, body').animate({
-			scrollTop: $('#count').position().top}, 'fast');
+			scrollTop: $('#count').position().top}, 'slow');
     });
  
 	$("#charts").click(function (){
 		$('html, body').animate({
-			scrollTop: $("#container").position().top -100 }, 'fast');
+			scrollTop: $("#container").position().top -100 }, 'slow');
 	});
 	$("#apply").click(function (){
 		$('html, body').animate({
-			scrollTop: $("#application").position().top -100 }, 'fast');
+			scrollTop: $("#application").position().top -100 }, 'slow');
 	});
 });
